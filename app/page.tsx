141b520caf7ef15b78d94b27072dbc9bf1ce7519
anil_akbay <anilakbay20@gmail.com>
2024-09-29 02:46:17 +0300

    projeye başlandı

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
 import Image from "next/image";
+import { PrismaClient } from "@prisma/client";
 
-export default function Home() {
+const db = new PrismaClient();
+
+export default async function Home() {
+  const allUsers = await db.user.findMany({
+    where: {
+      userAge: 10,
+    },
+    orderBy: {
+      userAge: "asc",
+    },
+    take: 10,
+    select: {
+      userAge: true,
+      email: true,
+    },
+  });
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
